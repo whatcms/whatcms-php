26c81738641e5cb6211d9ed882e3e1122674351c
Denis Efremov <efremov.a.denis@gmail.com>
2018-02-04 23:02:33 +0300

    Update WhatCMS.php

--- src/WhatCMS.php
+++ src/WhatCMS.php
@@ -1,55 +1,70 @@
-<?php
+<?php namespace WhatCMS;
 
-namespace WhatCMS;
+use GuzzleHttp\Client;
 
 /**
  * Base class for WhatCMS API wrapper
- *
  */
-class WhatCMS{
-
-	protected $api_key		= '';
-	protected $endpoint		= 'https://whatcms.org/APIEndpoint';
-
+class WhatCMS
+{
+    
+    /**
+     * Your WhatCMS API key.
+     *
+     * @var string
+     */
+	protected $key;
+    
+    /**
+     * WhatCMS API endpoint.
+     *
+     * @var string
+     */
+	protected $endpoint = 'https://whatcms.org/APIEndpoint';
+    
+    /**
+     * HTTP client instance.
+     *
+     * @var Client
+     */    
+    protected $client;
 
 	/**
-	 * Initialize WhatCMS
+	 * Create a new WhatCMS instance.
 	 *
+     * @param string $key Your API key
 	 */
-	public function __construct($api_key){
-
-		$this->api_key = $api_key;
-
+	public function __construct(string $key)
+    {
+		$this->key    = $key;
+        $this->client = new Client();
 	}
-
 
 	/**
-	 * Check for a CMS at the given url
-	 * If the api request fails, guzzle may throw an exception
+	 * Check for a CMS at the given URL.
+	 * If the API request fails, guzzle may throw an exception.
 	 *
-	 * @param string $check_url The url you would like to check for a CMS. Example: https://whatcms.org
-	 * @param bool $assoc Whether or not to return the result as an associative array. Defaults to true
-	 *
-	 * return mixed Returns NULL if the json response cannot be decoded. Returns associate array if the $assoc argument is true. Otherwise, returns object
+     * @see                  https://whatcms.org
+	 * @param  string $url   The url you would like to check for a CMS.
+	 * @param  bool   $array Whether or not to return the result as an associative array or object.
+	 * @return mixed         NULL if the json response cannot be decoded.
 	 */
-	public function CheckUrl($check_url, $assoc = true){
+	public function detect($url, $array = true)
+    {
+		$query = http_build_query(
+            array(
+                'key' => $this->key,
+                'url' => $url
+            ),
+            null,
+            '&',
+            PHP_QUERY_RFC3986
+        );
 
-		$query			= array();
-		$query['key']	= $this->api_key;
-		$query['url']	= $check_url;
+		$response = $this->client->request('GET', "{$this->endpoint}?{$query}");
+		$content  = $response->getBody()->__toString();
 
-		$url			= $this->endpoint . '?'. http_build_query($query, null,'&', PHP_QUERY_RFC3986 );
-
-		$client			= new \GuzzleHttp\Client();
-		$response		= $client->request('GET', $url );
-
-		$content		= $response->getBody()->__toString();
-		$result			= json_decode($content, $assoc);
-
-		return $result;
+		return json_decode($content, $array);
 	}
-
-
 
 }
-
